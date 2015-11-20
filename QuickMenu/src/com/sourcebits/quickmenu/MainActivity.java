6a2c1d623c155b3d0b87a8f03790f9648757a9fe
sourcebits-sunava <sunava.pattanayak@sourcebits.com>
2015-11-20 18:33:07 +0530

    My menu and quick menu
    mini project

---
+++ QuickMenu/src/com/sourcebits/quickmenu/MainActivity.java
@@ -0,0 +1,127 @@
+package com.sourcebits.quickmenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity 
+{
+	private PackageManager packageManager = null;
+	private List<ApplicationInfo> applist = null;
+	private ApplicationAdapter listadaptor = null;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		packageManager = getPackageManager();
+
+		new LoadApplications().execute();
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) 
+	{
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+
+		ApplicationInfo app = applist.get(position);
+		try {
+			Intent intent = packageManager
+					.getLaunchIntentForPackage(app.packageName);
+
+			if (null != intent) {
+				startActivity(intent);
+			}
+		} catch (ActivityNotFoundException e) {
+			Toast.makeText(MainActivity.this, e.getMessage(),
+					Toast.LENGTH_LONG).show();
+		} catch (Exception e) {
+			Toast.makeText(MainActivity.this, e.getMessage(),
+					Toast.LENGTH_LONG).show();
+		}
+	}
+
+	private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+		ArrayList<ApplicationInfo> applist = new ArrayList<ApplicationInfo>();
+		for (ApplicationInfo info : list) {
+			try {
+				if (null != packageManager.getLaunchIntentForPackage(info.packageName)) {
+					applist.add(info);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return applist;
+	}
+
+	private class LoadApplications extends AsyncTask<Void, Void, Void> {
+		private ProgressDialog progress = null;
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			applist = checkForLaunchIntent(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+			listadaptor = new ApplicationAdapter(MainActivity.this,
+					R.layout.list_row, applist);
+
+			return null;
+		}
+
+		@Override
+		protected void onCancelled() {
+			super.onCancelled();
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			setListAdapter(listadaptor);
+			progress.dismiss();
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute() {
+			progress = ProgressDialog.show(MainActivity.this, null,
+					"Loading application info...");
+			super.onPreExecute();
+		}
+
+		@Override
+		protected void onProgressUpdate(Void... values) {
+			super.onProgressUpdate(values);
+		}
+	}
+}
